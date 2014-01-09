b296b331e58c2eb4152f21153395b9c70fb38725
Ugnich Anton <anton@ugnich.com>
2014-01-09 14:36:20 +0700

    * JuickMessagesAdapter: new messages look

--- src/com/juick/android/JuickMessagesAdapter.java
+++ src/com/juick/android/JuickMessagesAdapter.java
@@ -17,7 +17,6 @@
  */
 package com.juick.android;
 
-import android.text.Layout.Alignment;
 import com.juick.android.api.JuickMessage;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -25,9 +24,7 @@ import android.graphics.Bitmap;
 import android.preference.PreferenceManager;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
-import android.text.style.AlignmentSpan;
 import android.text.style.ForegroundColorSpan;
-import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -55,17 +52,15 @@ public class JuickMessagesAdapter extends ArrayAdapter<JuickMessage> {
     public static Pattern urlPattern = Pattern.compile("((?<=\\A)|(?<=\\s))(ht|f)tps?://[a-z0-9\\-\\.]+[a-z]{2,}/?[^\\s\\n]*", Pattern.CASE_INSENSITIVE);
     public static Pattern msgPattern = Pattern.compile("#[0-9]+");
 //    public static Pattern usrPattern = Pattern.compile("@[a-zA-Z0-9\\-]{2,16}");
+    private ImageCache userpics;
     private ImageCache photos;
     private boolean usenetwork = false;
-    private String Replies;
     private int type;
-    private boolean allItemsEnabled = true;
     private SharedPreferences sp;
     private float textSize;
 
     public JuickMessagesAdapter(Context context, int type) {
         super(context, R.layout.listitem_juickmessage);
-        Replies = context.getResources().getString(R.string.Replies_) + " ";
         this.type = type;
 
         sp = PreferenceManager.getDefaultSharedPreferences(context);
@@ -82,6 +77,7 @@ public class JuickMessagesAdapter extends ArrayAdapter<JuickMessage> {
         }
 
         photos = new ImageCache(context, "photos-small", 1024 * 1024 * 5);
+        userpics = new ImageCache(context, "userpics-small", 1024 * 1024 * 2);
     }
 
     public int parseJSON(String jsonStr) {
@@ -108,10 +104,30 @@ public class JuickMessagesAdapter extends ArrayAdapter<JuickMessage> {
             if (v == null || !(v instanceof LinearLayout)) {
                 LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 v = vi.inflate(R.layout.listitem_juickmessage, null);
+                ((TextView) v.findViewById(R.id.text)).setTextSize(textSize);
+                if (type == TYPE_THREAD) {
+                    v.findViewById(R.id.comment).setVisibility(View.GONE);
+                    v.findViewById(R.id.replies).setVisibility(View.GONE);
+                }
             }
-            TextView t = (TextView) v.findViewById(R.id.text);
-            t.setTextSize(textSize);
 
+            ImageView upic = (ImageView) v.findViewById(R.id.userpic);
+            Bitmap bitmapupic = photos.getImageMemory(Integer.toString(jmsg.User.UID));
+            if (bitmapupic != null) {
+                upic.setImageBitmap(bitmapupic);
+            } else {
+                upic.setImageResource(R.drawable.ic_user_32);
+                ImageLoaderTask task = new ImageLoaderTask(userpics, upic, usenetwork);
+                task.execute(Integer.toString(jmsg.User.UID), "https://i.juick.com/as/" + jmsg.User.UID + ".png");
+            }
+
+            TextView username = (TextView) v.findViewById(R.id.username);
+            username.setText(jmsg.User.UName);
+
+            TextView timestamp = (TextView) v.findViewById(R.id.timestamp);
+            timestamp.setText(formatMessageTimestamp(jmsg));
+
+            TextView t = (TextView) v.findViewById(R.id.text);
             if (type == TYPE_THREAD && jmsg.RID == 0) {
                 t.setText(formatFirstMessageText(jmsg));
             } else {
@@ -124,24 +140,23 @@ public class JuickMessagesAdapter extends ArrayAdapter<JuickMessage> {
                 Bitmap bitmap = photos.getImageMemory(key);
                 if (bitmap != null) {
                     p.setImageBitmap(bitmap);
-                    p.setVisibility(View.VISIBLE);
                 } else {
-                    p.setVisibility(View.GONE);
+                    p.setImageResource(R.drawable.ic_attach_photo);
                     ImageLoaderTask task = new ImageLoaderTask(photos, p, usenetwork);
                     task.execute(key, jmsg.Photo);
                 }
+                p.setVisibility(View.VISIBLE);
             } else {
                 p.setVisibility(View.GONE);
             }
 
-            /*
-            ImageView i = (ImageView) v.findViewById(R.id.icon);
-            if (jmsg.User != null && jmsg.User.Avatar != null) {
-            i.setImageDrawable((Drawable) jmsg.User.Avatar);
+            if (jmsg.replies > 0 && type != TYPE_THREAD) {
+                TextView replies = (TextView) v.findViewById(R.id.replies);
+                replies.setVisibility(View.VISIBLE);
+                replies.setText(Integer.toString(jmsg.replies));
             } else {
-            i.setImageResource(R.drawable.ic_user_32);
+                v.findViewById(R.id.replies).setVisibility(View.GONE);
             }
-             */
 
         } else {
             if (v == null || !(v instanceof TextView)) {
@@ -168,46 +183,34 @@ public class JuickMessagesAdapter extends ArrayAdapter<JuickMessage> {
     }
 
     public void addDisabledItem(String txt, int position) {
-        allItemsEnabled = false;
         JuickMessage jmsg = new JuickMessage();
         jmsg.Text = txt;
         insert(jmsg, position);
     }
 
+    private String formatMessageTimestamp(JuickMessage jmsg) {
+        DateFormat df = new SimpleDateFormat("HH:mm dd/MMM/yy");
+        df.setTimeZone(TimeZone.getDefault());
+        return df.format(jmsg.Timestamp);
+    }
+
     private SpannableStringBuilder formatMessageText(JuickMessage jmsg) {
         SpannableStringBuilder ssb = new SpannableStringBuilder();
-        String name = '@' + jmsg.User.UName;
-        String tags = jmsg.getTags();
-        String txt = jmsg.Text;
-        if (jmsg.Photo != null) {
-            txt = jmsg.Photo + "\n" + txt;
-        }
-        if (jmsg.Video != null) {
-            txt = jmsg.Video + "\n" + txt;
-        }
-        ssb.append(name + ' ' + tags + "\n" + txt);
-        ssb.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), 0, name.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ssb.setSpan(new ForegroundColorSpan(0xFFC8934E), 0, name.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        if (tags.length() > 0) {
-            ssb.setSpan(new ForegroundColorSpan(0xFF0000CC), name.length() + 1,
-                    name.length() + tags.length() + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-
-        int paddingt = name.length() + 1 + tags.length() + 1;
+        ssb.append(jmsg.Text);
 
         // Highlight links http://example.com/
         int pos = 0;
-        Matcher m = urlPattern.matcher(txt);
+        Matcher m = urlPattern.matcher(jmsg.Text);
         while (m.find(pos)) {
-            ssb.setSpan(new ForegroundColorSpan(0xFF0000CC), paddingt + m.start(), paddingt + m.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            ssb.setSpan(new ForegroundColorSpan(0xFF0000CC), m.start(), m.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             pos = m.end();
         }
 
         // Highlight messages #1234
         pos = 0;
-        m = msgPattern.matcher(txt);
+        m = msgPattern.matcher(jmsg.Text);
         while (m.find(pos)) {
-            ssb.setSpan(new ForegroundColorSpan(0xFF0000CC), paddingt + m.start(), paddingt + m.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            ssb.setSpan(new ForegroundColorSpan(0xFF0000CC), m.start(), m.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             pos = m.end();
         }
 
@@ -216,60 +219,22 @@ public class JuickMessagesAdapter extends ArrayAdapter<JuickMessage> {
         pos = 0;
         m = usrPattern.matcher(txt);
         while (m.find(pos)) {
-        ssb.setSpan(new ForegroundColorSpan(0xFF0000CC), paddingt + m.start(), paddingt + m.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(new ForegroundColorSpan(0xFF0000CC), m.start(), m.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         pos = m.end();
         }
          */
-
-        DateFormat df = new SimpleDateFormat("HH:mm dd/MMM/yy");
-        df.setTimeZone(TimeZone.getDefault());
-        String date = df.format(jmsg.Timestamp);
-        ssb.append("\n" + date + " ");
-
-        int padding = name.length() + 1 + tags.length() + 1 + txt.length() + 1;
-        int end = padding + date.length() + 1;
-
-        ssb.setSpan(new ForegroundColorSpan(0xFFAAAAAA), padding, padding + date.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-        if (jmsg.replies > 0) {
-            String replies = Replies + jmsg.replies;
-            ssb.append("  " + replies + " ");
-            end += 2 + replies.length() + 1;
-            int padding2 = padding + date.length() + 1 + 2;
-            ssb.setSpan(new ForegroundColorSpan(0xFFC8934E), padding2, padding2 + replies.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-
-        ssb.setSpan(new AlignmentSpan.Standard(Alignment.ALIGN_OPPOSITE), padding, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         return ssb;
     }
 
     private SpannableStringBuilder formatFirstMessageText(JuickMessage jmsg) {
-        SpannableStringBuilder ssb = new SpannableStringBuilder();
+        SpannableStringBuilder ssb = formatMessageText(jmsg);
         String tags = jmsg.getTags();
         if (tags.length() > 0) {
-            tags += "\n";
+            int padding = ssb.length();
+            ssb.append("\n" + tags);
+            ssb.setSpan(new ForegroundColorSpan(0xFF999999), padding, padding + 1 + tags.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
-        String txt = jmsg.Text;
-        if (jmsg.Photo != null) {
-            txt = jmsg.Photo + "\n" + txt;
-        }
-        if (jmsg.Video != null) {
-            txt = jmsg.Video + "\n" + txt;
-        }
-        ssb.append(tags + txt);
-        if (tags.length() > 0) {
-            ssb.setSpan(new ForegroundColorSpan(0xFF000099), 0, tags.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-
-        int paddingt = tags.length();
-        int pos = 0;
-        Matcher m = urlPattern.matcher(txt);
-        while (m.find(pos)) {
-            ssb.setSpan(new ForegroundColorSpan(0xFF0000CC), paddingt + m.start(), paddingt + m.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            pos = m.end();
-        }
-
         return ssb;
     }
 }
