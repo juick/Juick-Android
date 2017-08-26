423a0180e8a31d1511a1d20872b14ecb6184fe58
gerc <mazahaka.09@inbox.ru>
2017-08-26 11:17:10 +0300

    Fix back button in new post screen

--- src/main/java/com/juick/android/NewMessageActivity.java
+++ src/main/java/com/juick/android/NewMessageActivity.java
@@ -47,7 +47,7 @@ public class NewMessageActivity extends BaseActivity implements ITagable {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
-        replaceFragment(NewPostFragment.newInstance());
+        addFragment(NewPostFragment.newInstance(), false);
     }
 
     @Override
