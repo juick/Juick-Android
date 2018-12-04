ba733a654df3773065d766aeba23bfe5e7471fea
Vitaly Takmazov <vitalyster@gmail.com>
2018-12-04 20:57:40 +0300

    Added internal crash reporting

--- src/main/java/com/juick/App.java
+++ src/main/java/com/juick/App.java
@@ -4,10 +4,15 @@ import android.app.Application;
 import android.content.Context;
 import android.support.v7.app.AppCompatDelegate;
 import android.util.TypedValue;
+import org.acra.ACRA;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraMailSender;
 
 /**
  * Created by gerc on 14.02.2016.
  */
+@AcraCore(buildConfigClass = BuildConfig.class)
+@AcraMailSender(mailTo = "support@juick.com", resSubject = R.string.appCrash)
 public class App extends Application {
 
     static {
@@ -24,5 +29,12 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+    }
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
     }
 }
