ef7ea91af7edf7547e5afa161742004952abb117
Vitaly Takmazov <vitalyster@gmail.com>
2020-02-03 13:01:33 +0300

    Update copyrights

--- src/main/java/com/juick/android/JuickMessagesAdapter.java
+++ src/main/java/com/juick/android/JuickMessagesAdapter.java
@@ -1,11 +1,10 @@
 /*
- * Juick
- * Copyright (C) 2008-2012, Ugnich Anton
+ * Copyright (C) 2008-2020, Juick
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
