947cca16cb0e13a0f02990160ad65d6a7cf7710b
Vitaly Takmazov <vitalyster@gmail.com>
2020-09-11 04:23:44 +0300

    Update copyrights

--- src/google/java/com/juick/android/service/FirebaseReceiverService.java
+++ src/google/java/com/juick/android/service/FirebaseReceiverService.java
@@ -23,9 +23,6 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.juick.App;
